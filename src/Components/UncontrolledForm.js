83fc072a93f7a6ed51742c0176cc280f27f18575
davidmchale <dmchale@squiz.net>
2023-11-08 10:33:42 +1100

    uncontrolled finished

--- src/Components/UncontrolledForm.js
+++ src/Components/UncontrolledForm.js
@@ -1,17 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useRef } from 'react';
 
 const UncontrolledForm = () => {
+
+  //refs MUST BE declared outside of functions like state etc
   const nameInputRef = useRef(null);
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
 
+
+
+  // functions
+  // =================
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Perform registration logic here
+
+    // Get the values from the rev
     const name = nameInputRef.current.value;
     const email = emailInputRef.current.value;
     const password = passwordInputRef.current.value;
+
+
+    const data = {
+      name, email, password
+    }
+
+    console.log(data)
+    // we can pass in a prop here from a parent conpoment to lift the data out
 
     // Reset input fields
     nameInputRef.current.value = '';
@@ -19,7 +34,6 @@ const UncontrolledForm = () => {
     passwordInputRef.current.value = '';
 
     // Additional registration logic
-    console.log(name, email, password)
   };
 
   return (
